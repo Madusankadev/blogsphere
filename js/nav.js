fa0435b70503f148875f7f15302eb64910ade58a
Ishara Madusanka <128658364+Madusankadev@users.noreply.github.com>
2024-07-11 15:13:15 +0530

    nav complete

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+
+function closeBtn() {
+    document.getElementById("mobileChangeNav").style.left = `100%`  
+     document.getElementById("body").style.overflowX = "hidden"  
+}
+
+function openBtn() {
+document.getElementById("mobileChangeNav").style.left =`0`
+document.getElementById("body").style.overflowX = "hidden"
+}
